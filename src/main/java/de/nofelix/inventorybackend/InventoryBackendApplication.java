5b701ee2eab5619fce523928044f1ef1a0daa579
Felix <69461004+no-felix@users.noreply.github.com>
2025-11-29 11:41:19 +0100

    🎉 Initial Commit

---
+++ src/main/java/de/nofelix/inventorybackend/InventoryBackendApplication.java
@@ -0,0 +1,13 @@
+package de.nofelix.inventorybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/nofelix/inventorybackend/InventoryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.nofelix.inventorybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
